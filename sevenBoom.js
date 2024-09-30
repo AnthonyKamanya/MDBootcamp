e0a966499a5279573a1903ba5f4a3f9b5295ecf1
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-09-30 16:18:17 +0100

    Add DaysOfTheWeek File to Session 3

---
+++ sevenBoom.js
@@ -0,0 +1,9 @@
+//Day 4 slide 78
+let numbers = [1, 2, 3, 5, 97];
+
+const sevenBoom = (number) => number === 7;
+if(numbers.some(sevenBoom)){
+    console.log("Boom")
+}else{
+    console.log("No booming here!")
+}
