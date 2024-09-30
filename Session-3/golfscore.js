27a8d8356f2e5bbe12ca5a98f8c70b787d376baf
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-09-30 16:21:15 +0100

    Add GolfScore File to Session 3

---
+++ Session-3/golfscore.js
@@ -0,0 +1,25 @@
+const shotNames = ["Hole in one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey" , "Quit"]; 
+const par = 0; 
+const strokes = 8;
+
+if (strokes == 1){
+    console.log(shotNames[0])
+}
+else if(strokes <= par - 2){
+    console.log(shotNames[1]);
+}
+else if(strokes == par - 1){
+    console.log(shotNames[2]);
+}
+else if(strokes == par){
+    console.log(shotNames[3]);
+}
+else if(strokes == par + 1 ){
+    console.log(shotNames[4]);
+}
+else if(strokes == par + 2){
+    console.log(shotNames[5]);
+}
+else{
+    console.log(shotNames[6])
+}
