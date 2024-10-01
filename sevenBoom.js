2d187967f2226cf761f73e2bb0abb660482be913
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-10-01 15:57:50 +0100

    Solve the SevenBoom problem

--- sevenBoom.js
+++ sevenBoom.js
@@ -1,7 +1,7 @@
 //Day 4 slide 78
 let numbers = [1, 2, 3, 5, 97];
 
-const sevenBoom = (number) => number === 7;
+const sevenBoom = (number) => number.toString().includes('7');
 if(numbers.some(sevenBoom)){
     console.log("Boom")
 }else{
