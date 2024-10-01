d6fcbf63f9bf4c1b916563a74907771bd7bf9966
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-10-01 15:58:42 +0100

    Create Function For Days Of The Week

---
+++ Session-3/daysoftheweek.js
@@ -0,0 +1,9 @@
+let daysOfTheWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+let indexOfDay = 5
+
+if(indexOfDay < 4 ){
+    console.log(`On a ${daysOfTheWeek[indexOfDay]} I go to the gym`)
+}
+else{console.log(`On a ${daysOfTheWeek[indexOfDay]} i do gardening`)
+};
