d086e7424fcfcf543d303b7087b78b2f0effb440
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-10-04 12:11:19 +0100

    Add Football Score Challenge to Session 7

---
+++ Session-7/footballScore.js
@@ -0,0 +1,5 @@
+// Write a program that calculates the number of points obtained by a football team over a given season​
+// A score is denoted by “X:Y” where X is the home score and Y is the away score. Assume we played our games at home all season.​
+// Input: an array of 38 strings.​
+// All score combinations should be covered.​
+// Bonus: Allow for your program to let the user state which teams points are required, i.e. home teams or the away teams.​
