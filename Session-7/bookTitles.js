22ae15a5b0524b46b2539c7fed2331e755a2c497
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-10-04 12:13:54 +0100

    Add BookTitles Challenge to Session 7

---
+++ Session-7/bookTitles.js
@@ -0,0 +1,3 @@
+// Write a function that sorts book titles in order of their complexity (see scenario in workbook).​
+// All strings provided contain only ASCII characters.​
+// Bonus: Add timer before final order is announced
