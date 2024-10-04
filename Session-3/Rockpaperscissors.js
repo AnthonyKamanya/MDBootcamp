6ece1db96453857bda60e46c70d033e397a2f4b8
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-10-04 12:07:27 +0100

    Add Question To The Solution To  Understand The Problem The Solution

---
+++ Session-3/Rockpaperscissors.js
@@ -0,0 +1,4 @@
+// Create a program that allows you to play rock, paper scissors. 
+// Print the outcome to the console window as well as 
+// Displaying the user choice and computer choice.​
+// Tip: Math.random() returns a value between 0 and 1 so this can be used to choose the computer's choice​
