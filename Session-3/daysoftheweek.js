812631bd460fc0cc644f2fc9d87d9d28cb6f1df0
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-10-04 12:03:45 +0100

    Add Question To The Solution File

--- Session-3/daysoftheweek.js
+++ Session-3/daysoftheweek.js
@@ -1,3 +1,9 @@
+// Given that a day is represented by a number and Monday is represented as the number 1. ​
+// Print out when the day is a weekday. ​
+// On a Tuesday I go to the gym. Display this. ​
+// On a Thursday I go to the gym. Display this. ​
+// On a Sunday I do gardening. Display this. 
+
 let daysOfTheWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
 let indexOfDay = 5
