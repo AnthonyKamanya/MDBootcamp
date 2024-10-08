298920688248c759847b20861ffe5ec13e914729
Anthony Kamanya <anthony.kamanya@autotrader.co.uk>
2024-10-08 06:23:52 +0100

    Add Solution Code to File

--- Session-6/temperatureConvertor.js
+++ Session-6/temperatureConvertor.js
@@ -7,3 +7,20 @@
 // To obtain temperature in Fahrenheit: (C * 1.8) + 32​
 // Write several tests to ensure the above calculations provide the correct result.​
 // You may use an online converter to check for expected results.​
+
+
+
+const temperatureCalcCel = (fahrenheit) =>{
+let celsius = (fahrenheit - 32)/ 9 * 5
+    celsius = Math.floor(celsius)
+console.log(`The temperature from  ${fahrenheit} fahrenheit is ${celsius} degree celsius`)
+} 
+
+const temperatureCalcFah = (celsius) =>{
+let fahrenheit = (celsius * 1.8 ) + 32
+    fahrenheit = Math.floor(fahrenheit)
+console.log(`The temperature from  ${celsius} celsius is ${fahrenheit} degree celsius`)
+}
+
+temperatureCalcCel(-45)
+temperatureCalcFah(20)
